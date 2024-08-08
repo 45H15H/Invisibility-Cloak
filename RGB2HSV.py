9ae9d797cf29aa1fbc90ac173b5e15080cba2cc8
Ashish Singh <ashish1qwerty1@gmail.com>
2024-08-08 19:45:33 +0530

    refactor: Update RGB to HSV conversion script

--- RGB2HSV.py
+++ RGB2HSV.py
@@ -2,9 +2,7 @@ import cv2
 import numpy as np
 
 # Define the RGB color
-# black_rgb = np.uint8([[[23, 19, 21]]]) # For black
-black_rgb = np.uint8([[[3, 4, 3]]]) # For black
-
+black_rgb = np.uint8([[[51, 12, 7]]]) # For black
 
 # Convert RGB to HSV
 black_hsv = cv2.cvtColor(black_rgb, cv2.COLOR_RGB2HSV)
